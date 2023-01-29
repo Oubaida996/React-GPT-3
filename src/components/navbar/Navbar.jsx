fed5e9a8374467267c9139408a2978f7b1b87223
Obieda Akilan <obieda.jehad@gmail.com>
2023-01-29 09:23:31 +0300

    Added assets folder, start work on navbar-links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import './navbar.css';
+import logo from '../../assets/logo.svg'
+
+import { RiMenu3Fill, RiCloseLine } from "react-icons/ri";
+
+/*  
+The way of name class called
+BEM --> Block Element Modifire  
+*/
 
 function Navbar () {
     return (
-        <div>Navbar</div>
+        <div className='gpt3__navbar'>
+            <div className="gpt3__navbar-links">
+                <div className="gpt3__navbar-links_logo">
+                    <img src={ logo } alt="logo.png" />
+                </div>
+                <div className="gpt3__navbar-links_container">
+                    <p><a href="#home">Home</a></p>
+                    <p><a href="#wgpt">What is GPT?</a></p>
+                    <p><a href="#possipility">Open AI</a></p>
+                    <p><a href="#Features">Case Studies</a></p>
+                    <p><a href="#blog">Library</a></p>
+                </div>
+            </div>
+        </div>
     )
 }
 

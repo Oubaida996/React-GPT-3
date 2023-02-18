24519c05550d33eb9a8f1ecd913642f48dada175
Obieda Akilan <obieda.jehad@gmail.com>
2023-02-18 17:20:25 +0300

    Added the Blog section

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './article.css';
 
-function Article () {
-    return (
-        <div>Article</div>
-    )
+function Article({ imageUrl, date, title }) {
+  return (
+    <div className='gpt3__blog-content__article'>
+      <div className='gpt3__blog-content__article-image'>
+        <img src={imageUrl} alt='blog.png' />
+      </div>
+      <div className='gpt3__blog-content__article-content'>
+        <div>
+          <p>{(date ||= 'Feb 26, 2023')}</p>
+          <h3>
+            {
+              (title ||=
+                'GPT-3 and Open AI is the future. Let us exlore how it is?')
+            }
+          </h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
 }
 
-export default Article
+export default Article;
--- src/container/blog/Blog.jsx
+++ src/container/blog/Blog.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import './blog.css';
+import { Article } from '../../components';
+import {
+  blog01,
+  blog02,
+  blog03,
+  blog04,
+  blog05,
+} from '../../components/article/import';
 
-function Blog () {
-    return (
-        <div>Blog</div>
-    )
+function Blog() {
+  return (
+    <div className='gpt3__blog section__padding'>
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container_groupA'>
+          <Article imageUrl={blog01} />
+        </div>
+        <div className='gpt3__blog-container_groupB'>
+          <Article imageUrl={blog02} />
+          <Article imageUrl={blog03} />
+          <Article imageUrl={blog04} />
+          <Article imageUrl={blog05} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Blog
+export default Blog;
